pmem/pmdk

common: add device dax support

src/libvmem/vmem.c
@@ -41,9 +41,11 @@
 #include <errno.h>
 #include <stdint.h>
 #include <pthread.h>
+#include <fcntl.h>
 
 #include "libvmem.h"
 
+#include "device_dax.h"
 #include "jemalloc.h"
 #include "pmemcommon.h"
 #include "sys_util.h"
@@ -150,12 +152,19 @@ vmem_create(const char *dir, size_t size)
 		return NULL;
 	}
 
+	int dax = device_dax_is_dax(dir);
+
 	/* silently enforce multiple of page size */
 	size = roundup(size, Pagesize);
 
 	void *addr;
-	if ((addr = util_map_tmpfile(dir, size, 4 << 20)) == NULL)
-		return NULL;
+	if (dax) {
+		if ((addr = device_dax_map(dir)) == NULL)
+			return NULL;
+	} else {
+		if ((addr = util_map_tmpfile(dir, size, 4 << 20)) == NULL)
+			return NULL;
+	}
 
 	/* store opaque info at beginning of mapped area */
 	struct vmem *vmp = addr;
@@ -167,7 +176,7 @@ vmem_create(const char *dir, size_t size)
 
 	/* Prepare pool for jemalloc */
 	if (je_vmem_pool_create((void *)((uintptr_t)addr + Header_size),
-			size - Header_size, 1) == NULL) {
+			size - Header_size, /* zeroed if */ !dax) == NULL) {
 		ERR("pool creation failed");
 		util_unmap(vmp->addr, vmp->size);
 		return NULL;
@@ -179,7 +188,8 @@ vmem_create(const char *dir, size_t size)
 	 * The prototype PMFS doesn't allow this when large pages are in
 	 * use. It is not considered an error if this fails.
 	 */
-	util_range_none(addr, sizeof(struct pool_hdr));
+	if (!dax)
+		util_range_none(addr, sizeof(struct pool_hdr));
 
 	LOG(3, "vmp %p", vmp);
 	return vmp;

wojtuss/nvml

src/libvmem/vmem.c
@@ -41,12 +41,14 @@
 #include <errno.h>
 #include <stdint.h>
 #include <pthread.h>
+#include <fcntl.h>
 
 #include "libvmem.h"
 
 #include "jemalloc.h"
 #include "pmemcommon.h"
 #include "sys_util.h"
+#include "file.h"
 #include "vmem.h"
 
 /*
@@ -150,12 +152,19 @@ vmem_create(const char *dir, size_t size)
 		return NULL;
 	}
 
+	int dax = util_file_is_device_dax(dir);
+
 	/* silently enforce multiple of page size */
 	size = roundup(size, Pagesize);
 
 	void *addr;
-	if ((addr = util_map_tmpfile(dir, size, 4 << 20)) == NULL)
-		return NULL;
+	if (dax) {
+		if ((addr = util_file_map_whole(dir)) == NULL)
+			return NULL;
+	} else {
+		if ((addr = util_map_tmpfile(dir, size, 4 << 20)) == NULL)
+			return NULL;
+	}
 
 	/* store opaque info at beginning of mapped area */
 	struct vmem *vmp = addr;
@@ -167,7 +176,7 @@ vmem_create(const char *dir, size_t size)
 
 	/* Prepare pool for jemalloc */
 	if (je_vmem_pool_create((void *)((uintptr_t)addr + Header_size),
-			size - Header_size, 1) == NULL) {
+			size - Header_size, /* zeroed if */ !dax) == NULL) {
 		ERR("pool creation failed");
 		util_unmap(vmp->addr, vmp->size);
 		return NULL;
@@ -179,7 +188,8 @@ vmem_create(const char *dir, size_t size)
 	 * The prototype PMFS doesn't allow this when large pages are in
 	 * use. It is not considered an error if this fails.
 	 */
-	util_range_none(addr, sizeof(struct pool_hdr));
+	if (!dax)
+		util_range_none(addr, sizeof(struct pool_hdr));
 
 	LOG(3, "vmp %p", vmp);
 	return vmp;